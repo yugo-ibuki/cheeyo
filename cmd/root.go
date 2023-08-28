6c48b38a18437e0929bdec45fc46801b5af2f647
yugo-ibuki <y.ibuki91@gmail.com>
2023-08-28 20:41:52 +0900

    fix: added option "path". if no path, then it is preparing the configured words

--- cmd/root.go
+++ cmd/root.go
@@ -5,39 +5,47 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/yugo-ibuki/cheeyo/pkg/cheer"
+	"github.com/yugo-ibuki/cheeyo/pkg/words"
 	"math/rand"
 	"os"
 	"time"
-)
-
-const (
-	dataPath = "./data.json"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "this cheers you up",
 	Short: "this cheers you up",
 	Run: func(cmd *cobra.Command, args []string) {
-		data, err := os.ReadFile(dataPath)
-		if err != nil {
-			fmt.Print("error occurs...: ", err)
-			os.Exit(1)
-		}
+		path := cmd.Flag("path").Value.String()
 
-		var words []string
-		if err := json.Unmarshal([]byte(data), &words); err != nil {
-			fmt.Print("error occurs...: ", err)
-			os.Exit(1)
+		var w []string
+		if len(path) != 0 {
+			fmt.Print("path: ", path)
+			data, err := os.ReadFile(path)
+			if err != nil {
+				fmt.Print("error occurs...: ", err)
+				os.Exit(1)
+			}
+
+			if err := json.Unmarshal([]byte(data), &w); err != nil {
+				fmt.Print("error occurs...: ", err)
+				os.Exit(1)
+			}
+		} else {
+			w = words.Words
 		}
 
 		// シード値の設定（一般的には現在時刻をシードとして使用する）
 		src := rand.NewSource(time.Now().UnixNano())
 		r := rand.New(src)
 		// 10から20の間のランダムな整数を取得
-		randomNumber := RandIntBetween(r, 0, len(words))
+		randomNumber := RandIntBetween(r, 0, len(w))
 
-		cheer.Print(words[randomNumber])
+		cheer.Print(w[randomNumber])
 	},
+}
+
+func init() {
+	rootCmd.Flags().StringP("path", "p", "", "words config path")
 }
 
 func Execute() {
